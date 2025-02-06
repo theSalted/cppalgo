acfcb1c0daa1bd75ee04fc79eef7ec4f07bb4efa
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-06 04:47:20 -0800

    anagram and duplicate

---
+++ duplicate/duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> numHashSet;
+        for (int num : nums) {
+            if (numHashSet.count(num) > 0) {
+                return true;
+            }
+            numHashSet.insert(num);
+        }
+        return false;
+    }
+};
+
