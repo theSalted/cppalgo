d988e38135b09fcac0701f9a0b4b692c2b81d9da
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-17 04:05:03 -0600

    k-most-frequent.cpp

---
+++ k-most-frequent/k-most-frequent.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+
+        for (auto num : nums) {
+            map[num]++;
+        }
+
+        // I didn't figure out this part myself
+        // According to chatgpt I can also use priority_queue here
+        vector<pair<int, int>> arr;
+        for (const auto& p : map) {
+            arr.push_back({p.second, p.first});
+        }
+
+        // rbegin: Return reverse iterator to reverse beginning
+        // rend: A reverse iterator to the reverse end of the sequence container
+        sort(arr.rbegin(), arr.rend());
+
+        vector<int> result;
+        for (int i = 0; i < k; i++) {
+            result.push_back(arr[i].second);
+        }
+
+        return result;
+    }
+};
+
