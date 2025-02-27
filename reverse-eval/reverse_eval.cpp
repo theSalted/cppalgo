9bf22443eeaf1c523f3632c054490a729d3fe7a2
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-02-27 15:47:16 -0800

    reverse_eval.cpp

---
+++ reverse-eval/reverse_eval.cpp
@@ -0,0 +1,54 @@
+
+[class Solution {
+public:
+    // I solved something just not rpn...
+    int evalReverse(vector<string>& tokens) {
+        int lhs = NULL;
+        int rhs = NULL;
+        
+        for (auto token : tokens) {
+            if (token == "+") {
+                int temp = lhs + rhs;
+
+                lhs = temp;
+                rhs = NULL;
+                continue;
+            }
+            
+            if (token == "-") {
+                int temp = lhs - rhs;
+                lhs = temp;
+                rhs = NULL;
+                continue;
+            }
+            
+            if (token == "*") {
+                int temp = lhs * rhs;
+                lhs = temp;
+                rhs = NULL;
+                continue;
+            }
+
+            if (token == "/") {
+                int temp = lhs / rhs;
+                lhs = temp;
+                rhs = NULL;
+                continue;
+            }
+
+            int num = stoi(token);
+
+            if (lhs == NULL) {
+                lhs = num;
+                continue;
+            }
+
+            if (rhs == NULL) {
+                rhs = num;
+                continue;
+            }
+        }
+
+        return lhs;
+    }
+};
